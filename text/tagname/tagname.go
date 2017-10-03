ba2a31d151c1f65d543f44b096875f2d159a1275
Alexsoul133 <alexsoul133@gmail.com>
2017-10-03 19:29:26 +1000

    tagname

--- text/tagname/tagname.go
+++ text/tagname/tagname.go
@@ -1,9 +1,26 @@
 package tagname
 
 import "github.com/macroblock/zl/core/zlog"
+import "strings"
 
+// Tagname - struct tagname
+type Tagname struct {
+	original                        []string
+	name, sdhd, terminator, mstring string
+	year, age, w, h                 int
+	qw, a                           []rune
+}
+
+var list []string
 var log = zlog.Instance("tagname")
 
+//Something - do smthng
 func Something() {
 	log.Warning(nil, "Something")
+}
+
+// New -
+func New(res string) *Tagname {
+	// &Tagname{original: strings.Split(res, "_")}
+	return &Tagname{original: strings.Split(res, "_")}
 }
--- text/tagname/test/test.go
+++ text/tagname/test/test.go
@@ -3,8 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/macroblock/zl/core/loglevel"
-
 	"github.com/macroblock/zl/core/zlog"
 	"github.com/macroblock/zl/core/zlogger"
 	"github.com/macroblock/zl/text/tagname"
@@ -17,11 +15,12 @@ func main() {
 		zlogger.Build().
 			Writer(os.Stdout).
 			Styler(zlogger.AnsiStyler).
-			LevelFilter(loglevel.Error.OrLower()).
+			// LevelFilter(loglevel.Error.OrLower()).
 			Format("(~m) ~l: ~x~e\n").
 			Done())
 	log.Info("programmstart")
 	log.Error(nil, "programmstart")
 	log.Warning(nil, "egega';,")
 	tagname.Something()
+	log.Info(tagname.New("sd_2015_Papashina_armiya__12_q0w2_trailer.mpg"))
 }
