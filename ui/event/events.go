f275c2f2a7584b72674f525749d513484583fc8e
mike <undefined.subject@gmail.com>
2017-10-18 00:20:58 +1000

    sync

--- ui/event/events.go
+++ ui/event/events.go
@@ -1,7 +1,47 @@
 package event
 
-// IEvent -
-type IEvent interface {
-	String() string
-	Key() string
+import "time"
+
+type (
+	// IEvent -
+	IEvent interface {
+		Time() time.Time
+		Type() string
+		EventKey() string
+		String() string
+	}
+
+	// TEvent -
+	TEvent struct {
+		IEvent
+		time time.Time
+	}
+)
+
+// Time -
+func (o *TEvent) Time() time.Time {
+	return o.time
+}
+
+// Type -
+func (o *TEvent) Type() string {
+	return "unknown"
+}
+
+// EventKey -
+func (o *TEvent) EventKey() string {
+	return ""
+}
+
+// String -
+func (o *TEvent) String() string {
+	return o.time.Format("15:04:05.000") + " " + o.IEvent.Type()
+}
+
+// TKeyboard -
+type TKeyboard struct {
+	TEvent
+	scan int
+	mod  int
+	ch   rune
 }
